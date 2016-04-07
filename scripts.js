aa80a7069fa3c08038d18570f3b18d995a7d5600
AndriyUkrajnets <aukrajnets@gmail.com>
2016-04-07 09:46:46 +0200

    item refactoring

--- scripts.js
+++ scripts.js
@@ -8,48 +8,56 @@ $( document ).ready(function() {
   $(document).on('click', '.abilities div', function(event){
     var currentType = $(event.target).html()
     if(displayedType == currentType){
-      $('.rectangle').show()
+      $('.item').show()
       displayedType = ''
-    } else {
-    $('.rectangle').hide()
-    $('.' + currentType).parents('.rectangle').show()
+    } 
+    else {
+    $('.item').hide()
+    $('.' + currentType).parents('.item').show()
     displayedType = currentType
-  }
+    }
   })
 });
+
 var renderProfile = function(event){
-  $("#rectangle-profile").html($(event.target).children(".hidden-info").html()).show();
+  $("#item-profile").html($(event.target).children(".hidden-info").html()).show();
 }
     
-  var statisticRow = function(label, value) {
+var statisticRow = function(label, value) {
   resalt = '<tr><td><font size="3px" face="Verdana" color="black";>'+label+
   '</font></td><td><font size="3px" face="Verdana" color="black";>'+value+'</font></td></tr>'
   return resalt; 
   }
+
 var content = '<img src="http://pokeapi.co/media/img/19.png">\
 <div>'
+
 var table = '</div>\
 <table border="1" style="width:100%">'
+
 var tableEnd = '</table>'
+
 var currentInfo={}
+
 var parseData = function(d){
-    currentInfo.name = d.name
-    currentInfo.weight = d.weight
-    types = []
-    for(var i = 0; i < d.types.length; i++) {
-      types[i] = d.types[i].type.name
-    }
-    currentInfo.types = types
-    currentInfo.totalMoves = d.moves.length
-    for(var i = 0; i < d.stats.length; i++) {
-      currentInfo[d.stats[i].stat.name] = d.stats[i].base_stat
-    }
+  currentInfo.name = d.name
+  currentInfo.weight = d.weight
+  types = []
+  for(var i = 0; i < d.types.length; i++) {
+    types[i] = d.types[i].type.name
   }
+  currentInfo.types = types
+  currentInfo.totalMoves = d.moves.length
+  for(var i = 0; i < d.stats.length; i++) {
+    currentInfo[d.stats[i].stat.name] = d.stats[i].base_stat
+  }
+}
+
 var  renderPokemonFromAPI = function(id){
 $.get( "http://pokeapi.co/api/v2/pokemon/"+id, function( data ) {
-    $( "#rectangle"+data.id+" .name").html( data.name );
+    $( "#item"+data.id+" .name").html( data.name );
     console.log(data); 
-    console.log("#rectangle"+data.id+" .name");
+    console.log("#item"+data.id+" .name");
   parseData(data)
   var profileImage = '<img src="http://pokeapi.co/media/img/'+data.id+'.png"><div>'
   var profileTable = table + 
@@ -63,7 +71,7 @@ $.get( "http://pokeapi.co/api/v2/pokemon/"+id, function( data ) {
     statisticRow('Weight', currentInfo.weight) + 
     statisticRow('Total moves', currentInfo.totalMoves) + 
     tableEnd
-  $( "#rectangle"+data.id+" .hidden-info" ).html( profileImage+data.name+" "+"#"+data.id+profileTable );
+  $( "#item"+data.id+" .hidden-info" ).html( profileImage+data.name+" "+"#"+data.id+profileTable );
   $(".pokemon").click(renderProfile);
   for(var y = 0; y < data.types.length; y++) {
       data.types[y].type.name
@@ -75,20 +83,23 @@ $.get( "http://pokeapi.co/api/v2/pokemon/"+id, function( data ) {
 };
 
 var newDivsStart = '\
-<div id="rectangle'
-var newDivsImage = '" class="rectangle pokemon">\
+<div id="item'
+
+var newDivsImage = '" class="item pokemon">\
 <img src="http://pokeapi.co/media/img/'
+
 var newDivsTypes = '.png" width="100" height="100">\
 <div class="name" align="center" style="width:auto; height:auto; position:relative; float:none;"></div>\
 <div class="abilities" id="ability-'
+
 var newDivsEnd = '"></div>\
 <div class="hidden-info"></div>\
 </div>\
 '
 function displ() {
-  var visiblePokemonsCount = $('.rectangle').length;
+  var visiblePokemonsCount = $('.item').length;
     for(var i = visiblePokemonsCount; i < (visiblePokemonsCount + 3); i++) {
-    $('#rectangle'+ i).after(newDivsStart + (i+1) + newDivsImage + (i+1) + newDivsTypes + (i+1) + newDivsEnd)
+    $('#item'+ i).after(newDivsStart + (i+1) + newDivsImage + (i+1) + newDivsTypes + (i+1) + newDivsEnd)
     renderPokemonFromAPI(i+1)
   }
 }
