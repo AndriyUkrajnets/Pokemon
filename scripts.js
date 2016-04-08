47fb1e94fa076809eaa50f898eab7e68cae87ae3
AndriyUkrajnets <aukrajnets@gmail.com>
2016-04-08 13:47:17 +0200

    Load more button

--- scripts.js
+++ scripts.js
@@ -1,11 +1,20 @@
-var displayedType = ''
+var displayedType = '';
 
-$( document ).ready(function() {
+$( document ).ready(function(){
   for(var i=1; i<19; i++){
    renderPokemonFromAPI(i)
   }
+
+/*"use strict";
+var displayedType = '';
+
+$( document ).ready(function(){
+  "use strict";
+  for(var i=1; i<19; i++){
+   renderPokemonFromAPI(i)
+  }*/
   $(".pokemon").click(renderProfile)
-  $(document).on('click', '.abilities div', function(event){
+  $(document).on('click', '.lable div', function(event){
     var currentType = $(event.target).html()
     if(displayedType == currentType){
       $('.item').show()
@@ -22,7 +31,7 @@ $( document ).ready(function() {
 var renderProfile = function(event){
   $("#item-profile").html($(event.target).children(".hidden-info").html()).show();
 }
-    
+
 var statisticRow = function(label, value) {
   resalt = '<tr><td><font size="3px" face="Verdana" color="black";>'+label+
   '</font></td><td><font size="3px" face="Verdana" color="black";>'+value+'</font></td></tr>'
@@ -54,40 +63,37 @@ var parseData = function(d){
 var  renderPokemonFromAPI = function(id){
 $.get( "http://pokeapi.co/api/v2/pokemon/"+id, function( data ) {
     $( "#item"+data.id+" .name").html( data.name );
-    console.log(data); 
+    console.log(data);
     console.log("#item"+data.id+" .name");
   parseData(data)
   var profileImage = '<img src="http://pokeapi.co/media/img/'+data.id+'.png" width="150" height="150"><div>'
-  var profileTable = table + 
-    statisticRow('Type', currentInfo.types[0]) + 
-    statisticRow('Attack', currentInfo.attack) + 
-    statisticRow('Defense', currentInfo.defense) + 
-    statisticRow('HP', currentInfo.hp) + 
-    statisticRow('SP Attack', currentInfo['special-attack']) + 
-    statisticRow('SP Defense', currentInfo['special-defense']) + 
-    statisticRow('Speed', currentInfo.speed) + 
-    statisticRow('Weight', currentInfo.weight) + 
-    statisticRow('Total moves', currentInfo.totalMoves) + 
+  var profileTable = table +
+    statisticRow('Type', currentInfo.types[0]) +
+    statisticRow('Attack', currentInfo.attack) +
+    statisticRow('Defense', currentInfo.defense) +
+    statisticRow('HP', currentInfo.hp) +
+    statisticRow('SP Attack', currentInfo['special-attack']) +
+    statisticRow('SP Defense', currentInfo['special-defense']) +
+    statisticRow('Speed', currentInfo.speed) +
+    statisticRow('Weight', currentInfo.weight) +
+    statisticRow('Total moves', currentInfo.totalMoves) +
     tableEnd
   $( "#item"+data.id+" .hidden-info" ).html( profileImage+data.name+" "+"#"+data.id+profileTable );
   $(".pokemon").click(renderProfile);
   for(var y = 0; y < data.types.length; y++) {
       data.types[y].type.name
-      prefContent = $("#ability-" + (data.id)).html()
+      prefContent = $("#lable-" + (data.id)).html()
       typeContent = prefContent + '<div class="'+ data.types[y].type.name +' type">'+ data.types[y].type.name +"</div>"
-      $("#ability-" + (data.id)).html(typeContent);
+      $("#lable-" + (data.id)).html(typeContent);
     };
   });
 };
 
-var newDivsStart = '\
-<div id="item'
+var newDivsStart = '<div id="item'
 
 var newDivsImage = '" class="item pokemon"><img src="http://pokeapi.co/media/img/'
 
-var newDivsTypes = '.png" width="100" height="100">\
-<div class="name"></div>\
-<div class="abilities" id="ability-'
+var newDivsTypes = '.png" width="100" height="100"><div class="name"></div><div class="lable" id="lable-'
 
 var newDivsEnd = '"></div><div class="hidden-info"></div></div>'
 function displ() {
